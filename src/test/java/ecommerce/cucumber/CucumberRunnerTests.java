e8ff76fc2f8fc6c5b3fbeb2c6894679b35c8179e
Mukaddes <mukaddes.a@outlook.com>
2024-02-15 19:17:59 +0300

    UI tests with cucumber-testng.

--- src/test/java/ecommerce/cucumber/CucumberRunnerTests.java
+++ src/test/java/ecommerce/cucumber/CucumberRunnerTests.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         plugin = "json:target/cucumber-report.json",
         features = "src/test/resources/features/login_checkout.feature",
-        glue="cucumber")
+        glue="ecommerce/cucumber")
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
 
 }
